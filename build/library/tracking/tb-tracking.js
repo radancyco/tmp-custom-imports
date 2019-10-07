862315280ecc5ec03423e594d5c028ed5815b5b9
Barnett <bbarnett@tmp.com>
2019-10-07 17:29:22 -0400

    getting ready to push live

--- build/library/tracking/tb-tracking.js
+++ build/library/tracking/tb-tracking.js
@@ -1,5 +1,4 @@
 // Look for any [data-custom-event="true"]
-// Question for Drew and Daniel. Is there a big enough performance concern to limit this to .js-tb-click-tracking[data-custom-event="true"], button[data-custom-event="true"], a[data-custom-event="true"]
 $('[data-custom-event="true"]').each(function(){
     $(this).removeAttr('data-custom-event'); // Removing attribute for TB Core does not fire it's script
     if( !$(this).hasClass('js-tb-click-tracking') ) {
@@ -8,18 +7,18 @@ $('[data-custom-event="true"]').each(function(){
 }).promise().done( function(){ // Promise will wait for the loop to complete 
 
     // Matches function is apart of the Custom Imports script removing during peer review
-    // if ( matches("?custom-debug", url) ) {
+    if ( matches("?custom-debug", url) ) {
         console.log('CI Debug - Tracking: No more data-custom-event attributes');
-    // }
+    }
 
     $('.js-tb-click-tracking').click(function(){ // now on anything with class of js-tb-click-tracking we will list for a click
             var cat = $(this).attr('data-custom-category');
             var label = $(this).attr('data-custom-label');
     if ( (typeof cat !== typeof undefined && cat !== false) && (typeof label !== typeof undefined && label !== false) ) { // Make sure Category and label exist
             APP.MODELS.GoogleBot.sendCustomDimensions(cat,'Click',label,'event');
-            // if ( matches("?custom-debug", url) ) {
+            if ( matches("?custom-debug", url) ) {
                 console.log('CI Debug - Tracking: ga fired');
-            // }
+            }
     } else { // if category or label are missing print a error to the console log
         var msg = $(this)[0].outerHTML;
         console.error("CI Error - Tracking: Missing 'data-custom-category' or 'data-custom-label' on: " + msg)
