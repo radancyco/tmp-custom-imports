6c82838299e682c9c3eb476d1a153bf0a3d044e8
Barnett <bbarnett@tmp.com>
2019-10-07 17:45:41 -0400

    Prevent GA from firing on preview and runmytests

--- build/library/tracking/tb-tracking.js
+++ build/library/tracking/tb-tracking.js
@@ -15,10 +15,18 @@ $('[data-custom-event="true"]').each(function(){
             var cat = $(this).attr('data-custom-category');
             var label = $(this).attr('data-custom-label');
     if ( (typeof cat !== typeof undefined && cat !== false) && (typeof label !== typeof undefined && label !== false) ) { // Make sure Category and label exist
+
+        if ( matches("preview.", url) || matches("runmytests.com", url) ) {
+            if ( matches("?custom-debug", url) ) {
+                console.log('CI Debug - TB Tracking: Category = ' + cat + ' Lablel = ' + label );
+            }
+        } else {
             APP.MODELS.GoogleBot.sendCustomDimensions(cat,'Click',label,'event');
             if ( matches("?custom-debug", url) ) {
                 console.log('CI Debug - TB Tracking: ga fired');
             }
+        }
+
     } else { // if category or label are missing print a error to the console log
         var msg = $(this)[0].outerHTML;
         console.error("CI Error - TB Tracking: Missing 'data-custom-category' or 'data-custom-label' on: " + msg)
