dbe99fd6ce3eeae277fe3a16d47d01e67c0ca7ef
Barnett <bbarnett@tmp.com>
2019-10-07 18:09:48 -0400

    Added a fail safe to check to make sure GA is working

--- build/library/tracking/tb-tracking.js
+++ build/library/tracking/tb-tracking.js
@@ -1,3 +1,6 @@
+var gaCheck = window[window['GoogleAnalyticsObject'] || 'ga']; // TODO: Since this is for TB only maybe adjust this for the TB verision of GA
+
+
 // Look for any [data-custom-event="true"]
 $('[data-custom-event="true"]').each(function(){
     $(this).removeAttr('data-custom-event'); // Removing attribute for TB Core does not fire it's script
@@ -16,20 +19,27 @@ $('[data-custom-event="true"]').each(function(){
             var label = $(this).attr('data-custom-label');
     if ( (typeof cat !== typeof undefined && cat !== false) && (typeof label !== typeof undefined && label !== false) ) { // Make sure Category and label exist
 
-        if ( matches("preview.", url) || matches("runmytests.com", url) ) {
-            if ( matches("?custom-debug", url) ) {
-                console.log('CI Debug - TB Tracking: Category = ' + cat + ' Lablel = ' + label );
+        if ( matches("preview.", url) || matches("runmytests.com", url) ) { // If you are on a preview site 
+            if ( matches("?tracking-debug", url) ) { // If the URL has ?tracking-debug create a popup when the tracked item is clicked
+                alert('CI Debug - TB Tracking: Category = ' + cat + ' Label = ' + label );
             }
-        } else {
-            APP.MODELS.GoogleBot.sendCustomDimensions(cat,'Click',label,'event');
-            if ( matches("?custom-debug", url) ) {
-                console.log('CI Debug - TB Tracking: ga fired');
+        } else { // If not on a preview site
+            if ( matches("?tracking-debug", url) ) { // If the URL has ?tracking-debug create a popup when the tracked item is clicked
+                alert('CI Debug - TB Tracking: Category = ' + cat + ' Label = ' + label );
+            }
+            if( typeof gaCheck == 'function' ) { // Check to make sure GA is running
+                APP.MODELS.GoogleBot.sendCustomDimensions(cat,'Click',label,'event'); // Fire the TB GA tracking 
+                if ( matches("?custom-debug", url) ) {
+                    console.log('CI Debug - TB Tracking: ga fired');
+                }
+            } else {
+                console.error("CI Error - TB Tracking: GA is not a function");
             }
         }
 
     } else { // if category or label are missing print a error to the console log
         var msg = $(this)[0].outerHTML;
-        console.error("CI Error - TB Tracking: Missing 'data-custom-category' or 'data-custom-label' on: " + msg)
+        console.error("CI Error - TB Tracking: Missing 'data-custom-category' or 'data-custom-label' on: " + msg);
     }
     })
 });
