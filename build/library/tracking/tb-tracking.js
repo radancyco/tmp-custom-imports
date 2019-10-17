b20a76ce9c8a87da7150defc1c476db61db0a8bb
Barnett <bbarnett@tmp.com>
2019-10-17 19:55:32 -0400

    Back to button and tracking changes

--- build/library/tracking/tb-tracking.js
+++ build/library/tracking/tb-tracking.js
@@ -15,11 +15,11 @@ if( $('[data-custom-event="true"]').length ) {
             console.log('CI Debug - TB Tracking: No more data-custom-event attributes');
         }
 
-        clickTrackingListener()
+        $('.js-tb-click-tracking').on('click', clickTrackingListener);
     });
 } else if( $('.js-tb-click-tracking').length ) {
 
-    clickTrackingListener()
+        $('.js-tb-click-tracking').on('click', clickTrackingListener);
 
 } else {
     // Since this script could be added in the theme level don't want to have this state an error
@@ -29,38 +29,38 @@ if( $('[data-custom-event="true"]').length ) {
 }
 
 function clickTrackingListener() { 
-    $('.js-tb-click-tracking').click(function(){ // now on anything with class of js-tb-click-tracking we will list for a click
+    // $('.js-tb-click-tracking').click(function(){ // now on anything with class of js-tb-click-tracking we will list for a click
         var cat = $(this).attr('data-custom-category');
         var label = $(this).attr('data-custom-label');
     if ( (typeof cat !== typeof undefined && cat !== false) && (typeof label !== typeof undefined && label !== false) ) { // Make sure Category and label exist
 
-    if ( matches("?custom-debug", url) ) { // If the URL has ?tracking-debug create a popup when the tracked item is clicked
-        alert('CI Debug - TB Tracking: Event Category = "' + cat + '" Event Label = "'+ label + '"' );
-    }
-
-    if ( matches("preview.", url) || matches("runmytests.com", url) ) { // If you are on a preview site 
-        if ( matches("?custom-debug", url) ) { // If the URL has ?tracking-debug create a popup when the tracked item is clicked
-            console.log('CI Debug - TB Tracking: On Preview')
-        }
-    } else { // If not on a preview site
         if ( matches("?tracking-debug", url) ) { // If the URL has ?tracking-debug create a popup when the tracked item is clicked
-            console.log('CI Debug - TB Tracking: On Live')
+            alert('CI Debug - TB Tracking: Event Category = "' + cat + '" Event Label = "'+ label + '"' );
         }
-        if( typeof gaCheck == 'function' ) { // Check to make sure GA is running
-            APP.MODELS.GoogleBot.sendCustomDimensions(cat,'Click',label,'event'); // Fire the TB GA tracking 
-            if ( matches("?custom-debug", url) ) {
-                console.log('CI Debug - TB Tracking: ga fired');
+
+        if ( matches("preview.", url) || matches("runmytests.com", url) ) { // If you are on a preview site 
+            if ( matches("?custom-debug", url) ) { // If the URL has ?tracking-debug create a popup when the tracked item is clicked
+                console.log('CI Debug - TB Tracking: On Preview')
             }
-        } else {
-            console.error("CI Error - TB Tracking: GA is not a function");
+        } else { // If not on a preview site
+            if ( matches("?custom-debug", url) ) { // If the URL has ?tracking-debug create a popup when the tracked item is clicked
+                console.log('CI Debug - TB Tracking: On Live')
+            }
+            if( typeof gaCheck == 'function' ) { // Check to make sure GA is running
+                APP.MODELS.GoogleBot.sendCustomDimensions(cat,'Click',label,'event'); // Fire the TB GA tracking 
+                if ( matches("?custom-debug", url) ) {
+                    console.log('CI Debug - TB Tracking: ga fired');
+                }
+            } else {
+                console.error("CI Error - TB Tracking: GA is not a function");
+            }
         }
-    }
 
     } else { // if category or label are missing print a error to the console log
-    var msg = $(this)[0].outerHTML;
-    console.error("CI Error - TB Tracking: Missing 'data-custom-category' or 'data-custom-label' on: " + msg);
+        var msg = $(this)[0].outerHTML;
+        console.error("CI Error - TB Tracking: Missing 'data-custom-category' or 'data-custom-label' on: " + msg);
     }
-    })
+    // })
 }
 
 // Hub Filter Tracking
