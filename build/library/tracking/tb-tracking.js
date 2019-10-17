a2f8fe403b18d201aa1e7e9777eaf7607f5ecb51
Barnett <bbarnett@tmp.com>
2019-10-17 19:58:40 -0400

    Removed and Renamed code

--- build/library/tracking/tb-tracking.js
+++ build/library/tracking/tb-tracking.js
@@ -15,11 +15,13 @@ if( $('[data-custom-event="true"]').length ) {
             console.log('CI Debug - TB Tracking: No more data-custom-event attributes');
         }
 
-        $('.js-tb-click-tracking').on('click', clickTrackingListener);
+        // now on anything with class of js-tb-click-tracking we will listen for a click
+        $('.js-tb-click-tracking').on('click', clickTrackingFire);
     });
 } else if( $('.js-tb-click-tracking').length ) {
 
-        $('.js-tb-click-tracking').on('click', clickTrackingListener);
+        // now on anything with class of js-tb-click-tracking we will listen for a click
+        $('.js-tb-click-tracking').on('click', clickTrackingFire);
 
 } else {
     // Since this script could be added in the theme level don't want to have this state an error
@@ -28,10 +30,9 @@ if( $('[data-custom-event="true"]').length ) {
     }
 }
 
-function clickTrackingListener() { 
-    // $('.js-tb-click-tracking').click(function(){ // now on anything with class of js-tb-click-tracking we will list for a click
-        var cat = $(this).attr('data-custom-category');
-        var label = $(this).attr('data-custom-label');
+function clickTrackingFire() { 
+    var cat = $(this).attr('data-custom-category');
+    var label = $(this).attr('data-custom-label');
     if ( (typeof cat !== typeof undefined && cat !== false) && (typeof label !== typeof undefined && label !== false) ) { // Make sure Category and label exist
 
         if ( matches("?tracking-debug", url) ) { // If the URL has ?tracking-debug create a popup when the tracked item is clicked
@@ -60,7 +61,6 @@ function clickTrackingListener() {
         var msg = $(this)[0].outerHTML;
         console.error("CI Error - TB Tracking: Missing 'data-custom-category' or 'data-custom-label' on: " + msg);
     }
-    // })
 }
 
 // Hub Filter Tracking
