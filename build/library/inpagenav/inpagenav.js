bc71877bd2ccc64ec7b24b1f2b14d55247881294
michaelspellacy <spell@spellacy.net>
2021-09-23 18:15:52 -0400

    Added update @drewhilltmp asked for.

--- build/library/inpagenav/inpagenav.js
+++ build/library/inpagenav/inpagenav.js
@@ -9,12 +9,12 @@ var inPageNavContentWrapper = document.getElementsByClassName("inPageNav__conten
 // inpage nav links
 var inPageNavLinks = document.querySelectorAll(".inPageNav__content a");
 // get callback function if it exists
-var inPageNavCallback = inPageNavButton.getAttribute("data-callback"); 
+var inPageNavCallback = inPageNavButton.getAttribute("data-callback");
 // get the first link and the last link for listeners
 var inPageNavLinkInitial = inPageNavLinks[0];
 if(inPageNavLinks){
   var inPageNavLinkCount = inPageNavLinks.length - 1;
-  var inPageNavLinkFinal = inPageNavLinks[inPageNavLinkCount];  
+  var inPageNavLinkFinal = inPageNavLinks[inPageNavLinkCount];
 }
 // variable to help know if initial setup has been completed
 var inPageNavSetup = 1;
@@ -28,7 +28,7 @@ var inPageNavHelper = {
     inPageNavHelper.createRelationships();
     // add tracking listener if data attribute exists
     if(inPageNav.hasAttribute("data-setup-ga")){
-      inPageNavHelper.setupGA();      
+      inPageNavHelper.setupGA();
     }
     // add breakpoint features
     inPageNavHelper.navWidthChanged();
@@ -55,7 +55,7 @@ var inPageNavHelper = {
     }
   },
   createRelationships: function(){
-    // what we need to do is create relationships between the menu nav links, 
+    // what we need to do is create relationships between the menu nav links,
     // the sections below, and build out return to nav
     for(var i = 0; i < inPageNavLinks.length; i++){
       var matchingSection = inPageNavLinks[i].getAttribute("href").replace("#","");
@@ -70,12 +70,12 @@ var inPageNavHelper = {
         returnToNavLink.classList.add("inPageNav__back-link");
         returnToNavLink.text = "Back to nav";
         returnToNavLink.setAttribute("href", "#nav-anchor-" + matchingSection);
-        returnToNav.appendChild(returnToNavLink);        
+        returnToNav.appendChild(returnToNavLink);
         matchingSectionExists.appendChild(returnToNav);
       } else{
         // remove the navigation link from the page because it's broken
         if(!inPageNavLinks[i].hasAttribute("ignore-link-check")){
-          inPageNavLinks[i].closest("li").remove();          
+          inPageNavLinks[i].closest("li").remove();
         }
       }
     }
@@ -85,15 +85,16 @@ var inPageNavHelper = {
     if(!(window.getComputedStyle(inPageNavButton).display === "none")){
       e.preventDefault();
       inPageNavButton.focus();
-      setTimeout(function(){ 
+      setTimeout(function(){
         inPageNavButton.click();
       }, 150);
-      setTimeout(function(){ 
+      setTimeout(function(){
          window.location.href = setTarget;
       }, 250);
     }
-    window[inPageNavCallback]("Back To Nav - " + setTarget);                      
-
+    if(inPageNavCallback.length) {
+      window[inPageNavCallback]("Back To Nav - " + setTarget);
+    }                   
   },
   setupGA: function(){
     // we might be able to update the existing links to include the data attributes
@@ -101,18 +102,18 @@ var inPageNavHelper = {
     for (var i = 0; i < inPageNavLinks.length; i++) {
       inPageNavLinks[i].setAttribute("data-custom-label", inPageNavLinks[i].innerHTML);
       inPageNavLinks[i].addEventListener("click", inPageNavHelper.fireGA);
-    } 
+    }
   },
   fireGA: function(){
     // set category and get text from link for label
     var setCat = "InPage Navigation";
     var setLabel = event.srcElement.getAttribute("data-custom-event");
     // below is where you would fire your GA call, internal ga call below won't work without custom package
-    if (typeof sendCustomDimensions === "function") { 
+    if (typeof sendCustomDimensions === "function") {
         // talentbrew analytics
         APP.MODELS.GoogleBot.sendCustomDimensions(setCat,'Click',setLabel,'event')
-    } 
-    
+    }
+
   },
   desktopSetup: function(){
     // change up items in prep for desktop mode
@@ -122,15 +123,15 @@ var inPageNavHelper = {
     inPageNavHelper.removeListeners();
 
     // call custom callback and send TYPE of event that happened
-    if(inPageNavCallback.length){ 
+    if(inPageNavCallback.length){
       if(inPageNavSetup == 1){
-        window[inPageNavCallback]("Initial - Desktop Setup");                      
+        window[inPageNavCallback]("Initial - Desktop Setup");
       } else{
-        window[inPageNavCallback]("Change - Desktop Setup");                      
+        window[inPageNavCallback]("Change - Desktop Setup");
       }
       // makes it so that Initial Setup isn't sent again as msg
       inPageNavSetup = 0;
-    }    
+    }
   },
   mobileSetup: function(){
     // change up items in prep for mobile mode
@@ -138,15 +139,15 @@ var inPageNavHelper = {
     inPageNavButton.setAttribute("aria-expanded", "false");
 
     // call custom callback and send TYPE of event that happened
-    if(inPageNavCallback.length){      
+    if(inPageNavCallback.length){
       if(inPageNavSetup == 1){
-        window[inPageNavCallback]("Initial - Mobile Setup");                      
+        window[inPageNavCallback]("Initial - Mobile Setup");
       } else{
-        window[inPageNavCallback]("Change - Mobile Setup");                      
+        window[inPageNavCallback]("Change - Mobile Setup");
       }
       // makes it so that Initial Setup isn't sent again as msg
-      inPageNavSetup = 0;      
-    }        
+      inPageNavSetup = 0;
+    }
   },
   inPageNavButtonClick: function(){
     // get current aria-expanded value
@@ -162,56 +163,56 @@ var inPageNavHelper = {
       inPageNavHelper.removeListeners();
     } else{
       // add listeners and set aria-expanded to true
-      inPageNavButton.setAttribute("aria-expanded","true");       
+      inPageNavButton.setAttribute("aria-expanded","true");
       callbackText = "Menu opened";
       inPageNavHelper.setupListeners();
     }
     // call custom callback and send TYPE of event that happened
-    if(inPageNavCallback.length){      
-      window[inPageNavCallback](callbackText);      
-    }    
+    if(inPageNavCallback.length){
+      window[inPageNavCallback](callbackText);
+    }
 
   },
   listenerTabFirst: function(e){
     // make sure the user isn't going backwards
     if(e.shiftKey &&  e.which === 9 ){
       //custom code here
-      inPageNavHelper.removeListeners();        
+      inPageNavHelper.removeListeners();
     }
   },
   listenerTabLast: function(e){
     // add event listener to first-link and if (shif+tab) close menu
     if(!e.shiftKey &&  e.which === 9 ){
       //custom code here
-      inPageNavHelper.removeListeners();        
+      inPageNavHelper.removeListeners();
     }
   },
-  listenerEscape: function(e){  
+  listenerEscape: function(e){
     // if escape key is pressed while menu is open on mobile
     if(e.key === "Escape" ){
-      //close all      
-      inPageNavHelper.removeListeners();        
+      //close all
+      inPageNavHelper.removeListeners();
     }
   },
   listenerOutsideClick: function(){
       var isClickInside = inPageNav.contains(event.target);
-      var isClickBurger = inPageNavButton.contains(event.target);      
-      if (!isClickInside && !isClickBurger) {        
+      var isClickBurger = inPageNavButton.contains(event.target);
+      if (!isClickInside && !isClickBurger) {
         // if user clicks outside of the navigation
-        inPageNavHelper.removeListeners();        
+        inPageNavHelper.removeListeners();
       }
   },
-  removeListeners: function(){        
+  removeListeners: function(){
     // removing event listeners from elements based on navigation needing to be closed on mobile
-    inPageNavButton.setAttribute("aria-expanded","false");       
-    inPageNav.removeEventListener('keydown', inPageNavHelper.listenerEscape);        
-    document.removeEventListener('click', inPageNavHelper.listenerOutsideClick);    
+    inPageNavButton.setAttribute("aria-expanded","false");
+    inPageNav.removeEventListener('keydown', inPageNavHelper.listenerEscape);
+    document.removeEventListener('click', inPageNavHelper.listenerOutsideClick);
     inPageNavLinkInitial.removeEventListener('keydown', inPageNavHelper.listenerTabFirst);
     inPageNavLinkFinal.removeEventListener('keydown', inPageNavHelper.listenerTabLast);
   },
   setupListeners: function(){
     // adding event listeners based on the navigation menu button being activated
-    inPageNav.addEventListener('keydown', inPageNavHelper.listenerEscape);        
+    inPageNav.addEventListener('keydown', inPageNavHelper.listenerEscape);
     document.addEventListener('click', inPageNavHelper.listenerOutsideClick);
     inPageNavLinkInitial.addEventListener('keydown', inPageNavHelper.listenerTabFirst);
     inPageNavLinkFinal.addEventListener('keydown', inPageNavHelper.listenerTabLast);
